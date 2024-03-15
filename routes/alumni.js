f2e042d0c860478e91a6e49e6a4bfa2df33fdb67
Aryan <ahujaaryan2511@gmail.com>
2024-03-15 14:28:35 +0530

    Added add alumni route

--- routes/alumni.js
+++ routes/alumni.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { connectToDatabase, handleErrors } = require('../common');
+const { ObjectId } = require('mongodb');
 
 router.get('/', async (req, res) => {
     try {
@@ -13,5 +14,32 @@ router.get('/', async (req, res) => {
         handleErrors(res, error);
     }
 });
+
+
+
+router.post('/add', async (req, res) => {
+    try {
+        const client = await connectToDatabase();
+        const database = client.db("database");
+        const collection = database.collection("Alumni");
+        const highestIdAlumni = await collection.findOne({}, { sort: { id: -1 }, limit: 1 });
+
+        const nextId = highestIdAlumni ? parseInt(highestIdAlumni.id) + 1 : 1;
+        const name = req.body.name
+        const already = await collection.findOne({ name });
+
+        if (already) {
+            res.status(400).json("Already Exists")
+        }
+        else {
+            const data = { ...req.body, id: nextId.toString() };
+            collection.insertOne(data);
+            res.status(200).json("Data added successfully");
+        }
+    } catch (error) {
+        handleErrors(res, error);
+    }
+});
+
 
 module.exports = router;
